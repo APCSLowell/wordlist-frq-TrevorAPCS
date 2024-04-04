4c6e5951a1a3352abca14583fdc3752587364aa6
TrevorAPCS <142834649+TrevorAPCS@users.noreply.github.com>
2024-04-04 11:48:46 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,12 +11,23 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     /* to be implemented in part (a) */
-    
+    int numWords = 0;
+    for(int i = 0; i < myList.size(); i++){
+      if(myList.get(i).length() == len){
+        numWords++;
+      }
+    }
+    return numWords;
   }
 
   public void removeWordsOfLength(int len)
   {
     /* to be implemented in part (b) */
-
+    for(int  i = 0; i < myList.size(); i++){
+      if(myList.get(i).length() == len){
+        myList.remove(i);
+        i--;
+      }
+    }
   }
 }
